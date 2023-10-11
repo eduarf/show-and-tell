940d72d1d3d12d5cd04eeaf1d62d2138592c88e8
eduarf <fraude_f@yahoo.com>
2023-10-11 16:57:52 +0300

    eventListRow responsive

--- src/components/event-lists/eventListRow/EventListRow.jsx
+++ src/components/event-lists/eventListRow/EventListRow.jsx
@@ -3,8 +3,7 @@ import { eventsItemsDetails } from "../../../../data";
 
 const StyledEventListRow = styled.div`
   margin-top: 6rem;
-  letter-spacing: -.6px;
-  font-weight: 600;
+  letter-spacing: -0.6px;
 `;
 
 const StyledList = styled.ul`
@@ -16,26 +15,62 @@ const StyledItem = styled.li`
   display: flex;
   justify-content: space-between;
   border-bottom: 2px solid black;
-  border-top: ${props => props.$firstItem ? '2px solid black' : 'none'};
+  border-top: ${(props) => (props.$firstItem ? "2px solid black" : "none")};
+  @media only screen and (max-width: 720px){
+    align-items: center;
+    padding: 2rem 0;
+  }
 `;
 
 const StyledItemEvent = styled.span`
-  width: 30%;
+  width: 35%;
+  @media only screen and (max-width: 720px){
+    width: 100%;
+  }
 `;
 
 const StyledItemShowDate = styled.span`
-  width: 30%;
-  margin-left: 2rem;
-  padding-left: 2rem;
+  width: 15%;
+  text-align: center;
+  @media only screen and (max-width: 720px){
+    width: 100%;
+    text-align: start;
+  }
 `;
 
-const StyledItemVenue = styled.span``;
+const StyledItemVenue = styled.span`
+  width: 30%;
+  padding-left: 10rem;
+  @media only screen and (max-width: 1100px) {
+    padding-left: 6rem;
+  }
+  @media only screen and (max-width: 1000px) {
+    padding-left: 4rem;
+  }
+  @media only screen and (max-width: 900px) {
+    padding-left: 2rem;
+  }
+  @media only screen and (max-width: 800px) {
+    padding-left: 0;
+  }
+  @media only screen and (max-width: 720px){
+    width: 100%;
+  }
+`;
 
 const StyledItemLeftContainer = styled.div`
-  width: 80%;
+  width: 85%;
   display: flex;
   gap: 2rem;
   align-items: center;
+  @media only screen and (max-width: 720px){
+    flex-direction: column;
+    width: 70%;
+    align-items: flex-start;
+    gap: .5rem;
+    margin-left: 2rem;
+    margin-right: 2rem;
+  }
 `;
 const StyledItemRightContainer = styled.div`
   display: flex;
@@ -47,23 +82,25 @@ const StyledItemRightContainer = styled.div`
 const StyledItemBuyNow = styled.a``;
 
 const StyledItemImg = styled.img`
-  height: 100%;
+  height: 5.5rem;
   object-fit: cover;
+  @media only screen and (max-width: 720px){
+    height: 6rem;
+  }
 `;
 
 const EventListRow = () => {
   return (
     <StyledEventListRow>
       <StyledList>
-        {eventsItemsDetails.map((item,index) => {
+        {eventsItemsDetails.map((item, index) => {
           const firstItem = index === 0;
           return (
             <StyledItem key={item.id} $firstItem={firstItem}>
+              <StyledItemImg src={item.photoSmall} />
               <StyledItemLeftContainer>
-                <StyledItemImg src={item.photoSmall} />
                 <StyledItemEvent>
-                  {item.group}  
-                  : {item.gamesName}
+                  {item.group}: {item.gamesName}
                 </StyledItemEvent>
                 <StyledItemShowDate>{item.date}</StyledItemShowDate>
                 <StyledItemVenue>{item.locality}</StyledItemVenue>
